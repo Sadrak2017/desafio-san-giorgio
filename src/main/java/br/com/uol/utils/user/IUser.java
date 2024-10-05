9b5469b5eff26b299286350a9df36be964716bb2
sadrak <sadrak_earth@outlook.com>
2024-10-04 21:56:49 -0300

    Remove classe obsoleta

--- src/main/java/br/com/uol/utils/user/IUser.java
+++ src/main/java/br/com/uol/utils/user/IUser.java
@@ -4,9 +4,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface IUser extends UserDetails {
   long getId();
-
-  IPatterns getPatterns() throws TPatternsRequiredException;
-
   boolean hasPatterns();
 
   boolean hasNrAccess();
